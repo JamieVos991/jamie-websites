d1d4f8d1307d4f6da6b483282e2345fe8d84f574
Jamie Vos <Jamievos100@gmail.com>
2026-03-16 16:53:59 +0100

    feat: prefers-color-scheme toegevoegd en een javascript knop waarmee je het thema kan wijzigen (#2)

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
-// Eventuele JS code
+const themeToggle = document.getElementById("theme-toggle");
+
+const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+document.body.classList.add(prefersDark ? "dark-mode" : "light-mode");
+
+themeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  document.body.classList.toggle("light-mode");
+});
